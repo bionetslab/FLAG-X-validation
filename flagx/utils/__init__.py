8c270b0e852d2418482dc4bdcf1a9f7d4f21db8e
Paul Martini <paul.martini@web.de>
2025-03-21 12:21:15 +0100

    add log10_trafo_w_cutoff_channel_wise()

--- flagx/utils/__init__.py
+++ flagx/utils/__init__.py
@@ -1,5 +1,5 @@
 
-from .utils import set_random_seed
+from .utils import set_random_seed, log10_trafo_w_cutoff_channel_wise
 
-__all__ = ['set_random_seed']
+__all__ = ['set_random_seed', 'log10_trafo_w_cutoff_channel_wise']
 
