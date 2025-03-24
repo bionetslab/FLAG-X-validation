b0a0202d5e4e83b8062ef24117ca369f6c5c8b30
Paul Martini <paul.martini@web.de>
2025-03-24 09:45:20 +0100

    add FCNNModel to imports

--- flagx/gating/__init__.py
+++ flagx/gating/__init__.py
@@ -1,7 +1,8 @@
 
 from .som_classifier import SomClassifier
 from .fcnn_softmax_classifier import SoftmaxClassifier
+from .fcnn_softmax_model import FCNNModel
 
-__all__ = ['SomClassifier', 'SoftmaxClassifier']
+__all__ = ['SomClassifier', 'SoftmaxClassifier', 'FCNNModel']
 
 
