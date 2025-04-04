09a9fa880b66a0a2a9668d32061c5edd82d93b21
Paul Martini <paul.martini@web.de>
2025-04-04 17:13:08 +0200

    add imports

--- flagx/dimred/__init__.py
+++ flagx/dimred/__init__.py
@@ -1,4 +1,6 @@
 
+from sklearn.decomposition import PCA
+from umap import UMAP
+from sklearn.manifold import TSNE, Isomap, LocallyLinearEmbedding, MDS, SpectralEmbedding
 
-
-
+__all__ = ['PCA', 'UMAP', 'TSNE', 'Isomap', 'LocallyLinearEmbedding', 'MDS', 'SpectralEmbedding']
