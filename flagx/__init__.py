309821f812004779de6128b29bb2da0a42257a08
Paul Martini <paul.martini@web.de>
2025-04-28 13:52:54 +0200

    add cli to imports

--- flagx/__init__.py
+++ flagx/__init__.py
@@ -5,5 +5,6 @@ from . import dimred
 from . import plt
 from . import utils
 from .pipeline import GatingPipeline
+from .cli import cli
 
-__all__ = ['io', 'gating', 'dimred', 'plt', 'GatingPipeline']
+__all__ = ['io', 'gating', 'dimred', 'plt', 'GatingPipeline', 'cli']
