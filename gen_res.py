a95b10ebe349043b847817d6084d204e5d947bdf
Paul Martini <paul.martini@web.de>
2025-10-06 13:13:49 +0200

    refactor

--- gen_res.py
+++ gen_res.py
@@ -1,27 +1,33 @@
 
 import argparse
 
-from main import main_som_classifier, main_dgcytof, main_softmax, main_gatemeclass
-
+from main import main_som_classifier, main_dgcytof, main_fcnn, main_gatemeclass
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="Run the scripts for generating results.")
+    parser = argparse.ArgumentParser(description='Run the scripts for generating results.')
+
+    methods = ['som', 'dgcytof', 'fcnn', 'gatemeclass']
 
     # Add the file argument
-    parser.add_argument('-m', type=str, help='The method for which to generate results')
+    parser.add_argument(
+        '-m', '--method',
+        type=str,
+        required=True,
+        choices=methods,
+        help=f'Select the method to run. Choices: {', '.join(methods)}'
+    )
 
     # Parse the arguments
     args = parser.parse_args()
 
-    if args.m == 'som':
-        main_som_classifier()
-    elif args.m == 'dgcytof':
-        main_dgcytof()
-    elif args.m == 'softmax':
-        main_softmax()
-    elif args.m == 'gatemeclass':
-        main_gatemeclass()
-    else:
-        print('Please specify a valid method.')
+    # Run script
+    method_to_script = {
+        'som': main_som_classifier,
+        'dgcytof': main_dgcytof,
+        'fcnn': main_fcnn,
+        'gatemeclass': main_gatemeclass,
+    }
+
+    method_to_script[args.method]()
