7bfcb4b5051cdf766b000273dde0bfb9abbdcffc
Paul Martini <paul.martini@web.de>
2025-10-15 16:59:35 +0200

    add fig3_iterative_refinement()

--- main_plotting.py
+++ main_plotting.py
@@ -452,8 +452,9 @@ def fig6_precision_recall():
     import seaborn as sns
 
     from matplotlib.ticker import FormatStrFormatter
-    from validation.utils import prec_rec_f1_avg_sample_wise
+    from validation.utils import eval_score_with_abstention
     from validation.plt import annotate_mosaic
+    from sklearn.metrics import precision_score, recall_score
 
     # ### Set flags and important variables here #######################################################################
     datasets = ['LT1b', 'LT2b']
@@ -468,8 +469,6 @@ def fig6_precision_recall():
     ####################################################################################################################
 
     if generate_plot_df:
-
-        pos_label = 1
 
         long_data = []
 
@@ -487,8 +486,8 @@ def fig6_precision_recall():
 
                 # Load the probabilistic predictions
                 pred_p = os.path.join(
-                    './results/probabilistic_pred',
-                    METHOD_TO_DIR[method],
+                    './results/probabilistic_predictions',
+                    METHOD_TO_DIR[method] + ('_20' if method == 'SOM-Classifier' else ''),
                     DATASET_TO_DIR[dataset],
                     'log10_w_custom_cutoffs/samples_y_proba'
                 )
@@ -503,28 +502,50 @@ def fig6_precision_recall():
                     # Get prediction for current threshold
                     samples_y_preds = [(y_prob >= threshold).astype(int) for y_prob in samples_y_proba]
 
-                    res_df_avg_prec, res_df_avg_rec, res_df_avg_f1 = prec_rec_f1_avg_sample_wise(
-                        y_trues=samples_y_test,
-                        y_preds=samples_y_preds,
-                        abstention_label=None,
-                        others_label=None,
-                        pos_label=pos_label,
-                        verbosity=0,
-                    )
+                    # Compute binary precision and recall for each sample
+                    precisions_binary = []
+                    recalls_binary = []
+                    for i, (yt, yp) in enumerate(zip(samples_y_test, samples_y_preds)):
 
+                        precisions_binary.append(
+                            eval_score_with_abstention(
+                                y_true=yt,
+                                y_pred=yp,
+                                eval_func=precision_score,
+                                eval_func_kwargs={'pos_label': 1, 'average': 'binary', 'zero_division': np.nan},
+                                abstention_label=-1 if method == 'SOM-Classifier' else None,
+                                others_label=None,
+                                verbosity=2,
+                            )
+                        )
+
+                        recalls_binary.append(
+                            eval_score_with_abstention(
+                                y_true=yt,
+                                y_pred=yp,
+                                eval_func=recall_score,
+                                eval_func_kwargs={'pos_label': 1, 'average': 'binary', 'zero_division': np.nan},
+                                abstention_label=-1 if method == 'SOM-Classifier' else None,
+                                others_label=None,
+                                verbosity=2,
+                            )
+                        )
+
+                    precisions_binary_ex_nan = [x for x in precisions_binary if not np.isnan(x)]
+                    recalls_binary_ex_nan = [x for x in recalls_binary if not np.isnan(x)]
                     long_data.append({
                         'Dataset': dataset,
                         'Method': method,
                         'Threshold': threshold,
-                        'Precision': res_df_avg_prec.loc['mean', 'binary'],
-                        'Recall': res_df_avg_rec.loc['mean', 'binary']
+                        'Precision': sum(precisions_binary_ex_nan) / len(precisions_binary_ex_nan),
+                        'Recall': sum(recalls_binary_ex_nan) / len(recalls_binary_ex_nan),
                     })
 
         plot_df = pd.DataFrame(long_data)
-        plot_df.to_csv('./results/probabilistic_pred/plot_df.csv')
+        plot_df.to_csv('./results/probabilistic_predictions/plot_df.csv')
 
     else:
-        plot_df = pd.read_csv('./results/probabilistic_pred/plot_df.csv', index_col=0)
+        plot_df = pd.read_csv('./results/probabilistic_predictions/plot_df.csv', index_col=0)
 
     plot_df_long = plot_df.melt(
         id_vars=['Dataset', 'Method', 'Threshold'],
@@ -534,7 +555,7 @@ def fig6_precision_recall():
     )
 
     # ### Plotting
-    fig = plt.figure(figsize=(8, 3), constrained_layout=True, dpi=300)
+    fig = plt.figure(figsize=(8, 4), constrained_layout=True, dpi=300)
     axd = fig.subplot_mosaic(
         """
         AB
@@ -595,42 +616,15 @@ def fig6_precision_recall():
     for ax in axd.values():
         if ax.get_legend():
             ax.get_legend().remove()
-    method_labels = ['FCNN', 'SOM-Classifier']
-    metric_labels = ['Precision', 'Recall']
-    method_handles = [h for h, l in zip(handles, labels) if l in method_labels]
-    metric_handles = [h for h, l in zip(handles, labels) if l in metric_labels]
-    method_legend = axd['L'].legend(
-        handles=method_handles,
-        labels=method_labels,
-        loc='center left',
-        title='Method',
-        fontsize=11,
-        title_fontsize=12,
-        frameon=True,
-    )
-    axd['L'].legend(
-        handles=metric_handles,
-        labels=metric_labels,
-        loc='center right',
-        title='Metric',
-        fontsize=11,
-        title_fontsize=12,
-        frameon=True,
-    )
-    axd['L'].add_artist(method_legend)
     axd['L'].axis('off')
-
-    # Todo: debug legend creation; if works and looks nice: adopt for other plots, if not use simple colmajor format
-
-    # axd['L'].axis('off')
-    # axd['L'].legend(
-    #     handles=handles,
-    #     labels=labels,
-    #     loc='center',
-    #     ncol=2,
-    #     fontsize=12,
-    #     frameon=True,
-    # )
+    axd['L'].legend(
+        handles=handles,
+        labels=labels,
+        loc='center',
+        ncol=2,
+        fontsize=10,
+        frameon=True,
+    )
 
     # Adjust font sizes
     ax_label_fontsize = 12
@@ -1310,7 +1304,7 @@ def fig3s_num_samples_num_events():
 
     annotate_mosaic(fig=fig, axd=axd, fontsize=16, excluded=['Z', ])
 
-    plt.savefig(os.path.join(PLOT_DIR, 'fig2s_num_samples_num_events.png'), dpi=fig.dpi)
+    plt.savefig(os.path.join(PLOT_DIR, 'fig3s_num_samples_num_events.png'), dpi=fig.dpi)
     plt.close('all')
 
 
@@ -1576,7 +1570,7 @@ def fig5s_minority_count():
 
     plot_num_events = False
 
-    generate_res_df = False
+    generate_res_df = True
 
     ####################################################################################################################
 
@@ -2025,8 +2019,7 @@ def fig6s_gating_performance_local():
     label_display_order = ['HSPC', 'M', 'M16', 'Ma', 'T', 'Th', 'NK', 'G', 'B', 'dyB', 'X', 'O']
 
     # Define a palette
-    methods = ['dummy0', 'dummy1'] + methods
-    palette = dict(zip(methods, sns.color_palette('Set2', len(methods))))
+    palette = dict(zip(['dummy0', 'dummy1'] + methods, sns.color_palette('Set2', len(methods) + 2)))
 
     # Initialize the mosaic
     fig = plt.figure(figsize=(8, 10), constrained_layout=True, dpi=300)
@@ -2052,7 +2045,7 @@ def fig6s_gating_performance_local():
             data_trafo = 'log10_w_custom_cutoffs' if dataset != 'Flowcyt' else 'log10_cutoff100'
             res_df_path = os.path.join(
                 base_path,
-                METHOD_TO_DIR[method],
+                METHOD_TO_DIR[method] + ('_20' if method == 'SOM-Classifier' else ''),
                 DATASET_TO_DIR[dataset],
                 data_trafo,
                 f'res_df_sw_avg_{performance_score}.csv'
@@ -2092,7 +2085,7 @@ def fig6s_gating_performance_local():
             data_trafo = 'log10_w_custom_cutoffs' if dataset != 'Flowcyt' else 'log10_cutoff100'
             res_df_path = os.path.join(
                 base_path,
-                METHOD_TO_DIR[method],
+                METHOD_TO_DIR[method] + ('_20' if method == 'SOM-Classifier' else ''),
                 DATASET_TO_DIR[dataset],
                 data_trafo,
                 f'res_df_sw_cw_{performance_score}.csv'
@@ -2341,11 +2334,312 @@ def _to_column_major(handles, ncol):
     return [h for h in reordered if h is not None]
 
 
+def fig3_iterative_refinement():
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    from matplotlib.patches import Polygon
+    from shapely.geometry import Point, Polygon as ShapelyPolygon
+    from flagx.io import FlowDataManager
+    from validation.plt import annotate_mosaic
+
+
+    fdm = FlowDataManager(
+        data_file_names=['annotated_train_data.fcs', ],  # ['annotated_train_data_downsampled_100000_events.fcs', ],
+        data_file_path='./results/pipeline_workflow/Imstat/output'
+    )
+    fdm.load_data_files_to_anndata()
+    adata = fdm.anndata_list_[0]
+
+    print(adata)
+    print(adata.var_names)
+
+    fig = plt.figure(figsize=(8, 8), constrained_layout=True, dpi=300)
+    axd = fig.subplot_mosaic(
+        '''
+        AB
+        CD
+        ''',
+        # gridspec_kw={'height_ratios': [1, 1]}
+    )
+
+    # --- Panel A: Ground truth gating in UMAP
+    ax = axd['A']
+
+    population = adata[:, 'population'].X.flatten()
+    margin = 2**20 * 0.05
+    min_val = margin
+    max_val = 2**20 - margin
+
+    original_min = 1
+    original_max = 8
+    original_range = original_max - original_min
+    original_population = ((population - min_val) / (max_val - min_val) * original_range + original_min).astype(int)
+
+    mask_nk_cells = (original_population == 3)
+
+    x_umap = adata[:, 'umap_1'].X.flatten()
+    y_umap = adata[:, 'umap_2'].X.flatten()
+
+    ax.scatter(
+        x=x_umap[mask_nk_cells],
+        y=y_umap[mask_nk_cells],
+        c='#4B9B69',
+        s=2,
+        linewidths=0.1,
+        edgecolors='darkgrey',
+        alpha=0.9,
+        zorder=2,
+        label='Ground Truth NK Cells'
+    )
+
+    ax.scatter(
+        x=x_umap[~mask_nk_cells],
+        y=y_umap[~mask_nk_cells],
+        c='lightgrey',
+        s=2,
+        linewidths=0.1,
+        edgecolors='darkgrey',
+        alpha=0.9,
+        zorder=1,
+        label='Others'
+    )
+
+    start_positions = [(750000, 650000), (660000, 330000), (960000, 420000)]
+    end_positions = [(660000, 550000), (600000, 450000), (830000, 420000)]
+
+    for start_position, end_position in zip(start_positions, end_positions):
+        ax.annotate(
+            '',  # no text
+            xy=end_position,
+            xytext=start_position,
+            arrowprops=dict(
+                arrowstyle='-|>,head_length=1,head_width=0.5',
+                color='crimson',
+                lw=4
+            )
+        )
+
+    find_arrow_pos = False
+    if find_arrow_pos:
+
+        xticks = np.arange(0, 1000000 + 1, 100000)
+        yticks = np.arange(0, 1000000 + 1, 100000)
+
+        ax.set_xticks(xticks)
+        ax.set_yticks(yticks)
+
+        for label in ax.get_xticklabels() + ax.get_yticklabels():
+            label.set_fontsize(6)
+
+        ax.grid(True, which='major', linestyle='-', color='black', alpha=0.9)
+
+    else:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    ax.set_xlabel('UMAP 1')
+    ax.set_ylabel('UMAP 2')
+
+    ax.legend(markerscale=5)
+
+    # --- Panel B: Ground truth gating in trained SOM
+    ax = axd['B']
+
+    x_som = adata[:, 'som_1'].X.flatten()
+    y_som = adata[:, 'som_2'].X.flatten()
+
+    ax.scatter(
+        x=x_som[mask_nk_cells],
+        y=y_som[mask_nk_cells],
+        c='#4B9B69',
+        s=2,
+        linewidths=0.1,
+        edgecolors='darkgrey',
+        alpha=0.9,
+        zorder=2,
+        label='Ground Truth NK Cells'
+    )
+
+    ax.scatter(
+        x=x_som[~mask_nk_cells],
+        y=y_som[~mask_nk_cells],
+        c='lightgrey',
+        s=2,
+        linewidths=0.1,
+        edgecolors='darkgrey',
+        alpha=0.9,
+        zorder=1,
+        label='Others'
+    )
+
+    start_positions = [(750000, 880000), (930000, 550000), (750000, 150000)]
+    end_positions = [(860000, 810000), (810000, 630000), (610000, 150000)]
+
+    for start_position, end_position in zip(start_positions, end_positions):
+        ax.annotate(
+            '',
+            xy=end_position,
+            xytext=start_position,
+            arrowprops=dict(
+                arrowstyle='-|>,head_length=1,head_width=0.5',
+                color='crimson',
+                lw=4
+            )
+        )
+
+    # Plot gate
+    poly_points = np.array([
+        [470000, 275000],
+        [430000, 275000],
+        [430000, 390000],
+        [390000, 390000],
+        [390000, 450000],
+        [450000, 510000],
+        [575000, 510000],
+        [660000, 415000],
+        [620000, 375000],
+        [620000, 320000],
+        [470000, 320000],
+    ])
+    poly = Polygon(
+        poly_points,
+        closed=True,
+        facecolor='none',
+        edgecolor='crimson',
+        linewidth=1
+    )
+    ax.add_patch(poly)
+
+    find_arrow_pos = False
+    if find_arrow_pos:
+
+        xticks = np.arange(0, 1000000 + 1, 50000)
+        yticks = np.arange(0, 1000000 + 1, 50000)
+
+        ax.set_xticks(xticks)
+        ax.set_yticks(yticks)
+
+        for label in ax.get_xticklabels() + ax.get_yticklabels():
+            label.set_fontsize(3)
+
+        ax.grid(True, which='major', linestyle='-', color='black', alpha=0.9)
+
+    else:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    ax.set_xlabel('SOM Grid 1')
+    ax.set_ylabel('SOM Grid 2')
+
+    ax.legend(markerscale=5, loc='upper left')
+
+    # --- Panel C: Gating to core NK population in SOM
+    ax = axd['C']
+
+    points = adata[:, ['som_1', 'som_2']].X
+
+    shapely_poly = ShapelyPolygon(poly_points)
+    mask_core_nk_population = np.array([
+        shapely_poly.contains(Point(p)) or shapely_poly.touches(Point(p)) for p in points
+    ])
+
+    ax.scatter(
+        x=x_som[mask_core_nk_population],
+        y=y_som[mask_core_nk_population],
+        c='#B94B4B',
+        s=2,
+        linewidths=0.1,
+        edgecolors='darkgrey',
+        alpha=0.9,
+        zorder=2,
+        label='Core NK Cells'
+    )
+
+    ax.scatter(
+        x=x_som[~mask_core_nk_population],
+        y=y_som[~mask_core_nk_population],
+        c='lightgrey',
+        s=2,
+        linewidths=0.1,
+        edgecolors='darkgrey',
+        alpha=0.9,
+        zorder=1,
+        label='Others'
+    )
+
+    # Plot gate
+    poly = Polygon(
+        poly_points,
+        closed=True,
+        facecolor='none',
+        edgecolor='crimson',
+        linewidth=1
+    )
+    ax.add_patch(poly)
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax.set_xlabel('SOM Grid 1')
+    ax.set_ylabel('SOM Grid 2')
+
+    ax.legend(markerscale=5, loc='lower left')
+
+    # --- Panel D: Core NK population in UMAP
+    ax = axd['D']
+
+    ax.scatter(
+        x=x_umap[mask_core_nk_population],
+        y=y_umap[mask_core_nk_population],
+        c='#B94B4B',
+        s=2,
+        linewidths=0.1,
+        edgecolors='darkgrey',
+        alpha=0.9,
+        zorder=2,
+        label='Core NK Cells'
+    )
+
+    ax.scatter(
+        x=x_umap[~mask_core_nk_population],
+        y=y_umap[~mask_core_nk_population],
+        c='lightgrey',
+        s=2,
+        linewidths=0.1,
+        edgecolors='darkgrey',
+        alpha=0.9,
+        zorder=1,
+        label='Others'
+    )
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax.set_xlabel('UMAP 1')
+    ax.set_ylabel('UMAP 2')
+
+    ax.legend(markerscale=5, loc='lower left')
+
+
+    annotate_mosaic(fig=fig, axd=axd, fontsize=16, excluded=None)
+
+    fig.savefig(os.path.join(PLOT_DIR, 'fig3_iterative_refinement.png'), dpi=fig.dpi)
+
+
+
 if __name__ == '__main__':
 
     os.makedirs(PLOT_DIR, exist_ok=True)
 
+    import matplotlib
+
+    matplotlib.use('Agg')
+
     # fig1_gating_performance()
+
+    # fig3_iterative_refinement()
 
     # fig5_num_samples()
 
@@ -2355,9 +2649,9 @@ if __name__ == '__main__':
 
     # fig2s_gating_performance_class_wise()
 
-    fig3s_num_samples_num_events()
+    # fig3s_num_samples_num_events()
 
-    # fig4s_sample_order()
+    # fig4s_sample_order()  # todo
 
     # fig5s_minority_count()
 
