f579d9edfcd7af9c5e8c504c4b872acc9a0bceb1
Paul Martini <paul.martini@web.de>
2025-11-21 17:03:08 +0100

    remove import of plotting module

--- flagx/__init__.py
+++ flagx/__init__.py
@@ -2,7 +2,6 @@
 from . import io
 from . import gating
 from . import dimred
-from . import plt
 from .pipeline import GatingPipeline
 
-__all__ = ['io', 'gating', 'dimred', 'plt', 'GatingPipeline']
+__all__ = ['io', 'gating', 'dimred', 'GatingPipeline']
